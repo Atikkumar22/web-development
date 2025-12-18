219548804d471e666da204f1ea9545df6dab6bf6
Atik Kumar <kumaratik424@gmail.com>
2025-12-18 18:39:22 +0530

    feat: add initial HTML and JavaScript files for React setup

---
+++ react/day1/injs.js
@@ -0,0 +1,21 @@
+// Getting started with react in js. 
+// using cdn files to work in react in html and js
+
+    //document ki jagah react likh hai : 1st- elements , 2nd- attributes, 3rd- content
+let h1 = React.createElement('h1' ,null, 'Main hu insaan' );
+let h2 = React.createElement('h2' ,null, 'Main hu danav' );
+
+//let container = document.querySelector('#container');
+
+//let root = ReactDOM.createRoot(container)
+
+let div = React.createElement('div', {id:'parent' , className:'elem'}, [h1,h2])
+
+    //merging line 8 and 10
+let root = ReactDOM.createRoot(document.querySelector('#container'));
+
+    //root.render(h1,h2) //root only accepts one argument otherwise it give error
+// root.render(h2)
+
+    //multiple cheeze agr root me render krni ho to , hum unko ek single div me rkhte h qki root single arg. based h
+root.render(div)    
