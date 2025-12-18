d8659d02e0f5fe781fadb222990013cac6bb0697
Atik Kumar <kumaratik424@gmail.com>
2025-12-18 17:11:44 +0530

    questions

--- javascript/Assignments/asgn.js
+++ javascript/Assignments/asgn.js
@@ -1,19 +1,35 @@
-// question 4
-let x=5 , y=10;
-console.log(x==y);
+// // question 4
+// let x=5 , y=10;
+// console.log(x==y);
 
-let c= "atik";
-console.log(c);
+// let c= "atik";
+// console.log(c);
 
-for(i=1 ; i<11 ;i++)
-{
-    console.log(i + "yo");
+// for(i=1 ; i<11 ;i++)
+// {
+//     console.log(i + "yo");
+// }
+
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<10)
+
+//2
+// console.log(a);
+// var a = 10;
+// function a() {}
+// console.log(a);
+
+//3
+// console.log('A');
+// setTimeout(() => console.log('B'), 0);
+// Promise.resolve().then(() => console.log('C'));
+// console.log('D')
+
+//4
+for (var i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 0);
 }
-
-let i=1;
-do{
-    console.log(i);
-    i++;
-}
-while(i<10)
-
