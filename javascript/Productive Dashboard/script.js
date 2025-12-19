4688e8031045732a0f0e7b8769fd8258facb35f3
Atik Kumar <kumaratik424@gmail.com>
2025-12-19 20:11:16 +0530

    created cards for techniques

---
+++ javascript/Productive Dashboard/script.js
@@ -0,0 +1,8 @@
+let allElems = document.querySelectorAll('.elem')
+
+allElems.forEach(function(elem){
+    elem.addEventListener('click', function(){
+        // console.log(elem.id);
+        console.log(document.querySelectorAll('.fullElem'))
+    })
+})
