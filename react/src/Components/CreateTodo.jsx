5374356100a3473e0169b38c709dac3a5af34901
Atik Kumar <kumaratik424@gmail.com>
2025-12-19 16:04:42 +0530

    feat: add all three component and integrate it into main rendering

---
+++ react/src/Components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateTodo = () => {
+    return (
+    <div>
+        <h2>I just created a rafce snippet by hand</h2>
+    </div>
+    )
+}
+
+export default CreateTodo
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Card from "./Components/Card.jsx";
+import Paragraph from "./Components/Paragraph.jsx";
+import CreateTodo from './Components/CreateTodo.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* <App /> */}
+    <Card />
+    <Paragraph />
+    <CreateTodo />
+  </StrictMode>
 )
