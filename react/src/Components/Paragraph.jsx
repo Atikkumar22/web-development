ef19292eb11bfa4419d7f2f4b84dc7320a200e35
Atik Kumar <kumaratik424@gmail.com>
2025-12-19 16:04:01 +0530

    created rafce component

---
+++ react/src/Components/Paragraph.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Paragraph = () => {
+  return (
+    <div>
+      <p>this is a paragraph that is made inside a rafce shorthand got react-arrow-function-component</p>
+    </div>
+  )
+}
+
+export default Paragraph
