35394e8e7d3470d01639b563faf6cbe67095914d
Atik Kumar <kumaratik424@gmail.com>
2025-12-20 12:45:23 +0530

    learning and correcting folder structure for better day by day react lectures

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import Card from "./Components/Card.jsx";
-import Paragraph from "./Components/Paragraph.jsx";
-import CreateTodo from './Components/CreateTodo.jsx';
+import Card from "./day2-Components/Card.jsx";
+import Paragraph from "./day2-Components/Paragraph.jsx";
+import CreateTodo from './day2-Components/CreateTodo.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
