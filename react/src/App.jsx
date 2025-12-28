8fee1d16155a0a6131bf266112a3c767410b1a01
Atik Kumar <kumaratik424@gmail.com>
2025-12-28 16:29:13 +0530

    fix: remove unnecessary blank lines in App.jsx imports

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,12 +4,12 @@ import Card from "./day2-Components/Card.jsx";
 import Paragraph from "./day2-Components/Paragraph.jsx";
 import CreateTodo from './day2-Components/CreateTodo.jsx';
 
+
 import Nav from './day3-structures/navbar/Nav.jsx';
 import Hero from './day3-structures/hero section/Hero.jsx';
 import Middle from './day3-structures/middle section/Middle.jsx';
 import Services from './day3-structures/services/Services.jsx';
 import './day3.css'
-
 
 
 import Prop from './day4-prop drilling/Prop.jsx';
