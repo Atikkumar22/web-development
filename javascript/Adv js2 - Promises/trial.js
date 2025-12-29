ca710f2b43a8caf9e89f2f1f2723812dd1c3e86f
Atik Kumar <kumaratik424@gmail.com>
2025-12-29 20:03:36 +0530

    promises example

---
+++ javascript/Adv js2 - Promises/trial.js
@@ -0,0 +1,21 @@
+
+// const myPromise = new Promise((resolve, reject) => {
+//   // Asynchronous operation here (e.g., API call, timeout)
+//   setTimeout(() => {
+//     const success = true; // Or false to simulate an error
+//     if (success) {
+//       resolve("Operation completed successfully!");
+//     } else {
+//       reject("Operation failed!");
+//     }
+//   }, 1000); // Simulate a 1-second delay
+// });
+
+// myPromise
+//   .then((result) => {
+//     console.log(result); // Handle successful result
+//   })
+//   .catch((error) => {
+//     console.error(error); // Handle errors
+//   });
+
