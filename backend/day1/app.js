11d9052b1ea06e211ef0c0de8a6c6c3e5cb6591c
Atik Kumar <kumaratik424@gmail.com>
2026-01-26 18:30:24 +0530

    starting to learn backend

---
+++ backend/day1/app.js
@@ -0,0 +1,7 @@
+const catMe = require("cat-me")
+
+console.log(catMe())
+
+//packages ek esa code jo kisi or ne likha or npm js ki website pe rakha diya h , pr usko uske krne ke liye hume packages ko install krna hoga , esa isliye krna h taki npm website pe jo code packages h wo apne system pe node modules me ayegnge or fir hum inko use kr skte h 
+
+//package-lock.json file manage krti h ki humne jo code likha h wo kin packages pe depened krta h or package further kin packages me depend krta h ...inhi intertuine package handling ko package-lock krta h
