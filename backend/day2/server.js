a3702b6cc58e50020e820a5ccc18be814d6a60fc
Atik Kumar <kumaratik424@gmail.com>
2026-01-27 17:25:50 +0530

    getting response at client side

--- backend/day2/server.js
+++ backend/day2/server.js
@@ -2,5 +2,10 @@ const express = require("express");
 
 const app = express() //server created , or uska instance server constant me agya h 
 
+//not adding line to get some response on the client  side from the server
+app.get('/', (req,res) => {
+    res.send('hello world!!')
+})
+
 app.listen(3000) //server ko start krta hai 
 console.log("server started on port 3000")
