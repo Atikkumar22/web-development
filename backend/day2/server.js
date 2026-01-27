bde4ceacb1fc51903314425ee38ad3fe11d6076a
Atik Kumar <kumaratik424@gmail.com>
2026-01-27 17:12:20 +0530

    minor change

--- backend/day2/server.js
+++ backend/day2/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const server = express() //server created , or uska instance server constant me agya h 
+const app = express() //server created , or uska instance server constant me agya h 
 
-server.listen(3000) //server ko start krta hai 
+app.listen(3000) //server ko start krta hai 
 console.log("server started on port 3000")
