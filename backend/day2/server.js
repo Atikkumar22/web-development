376dc986cade0d268fbff2351592c27dc6fd1f1b
Atik Kumar <kumaratik424@gmail.com>
2026-01-29 19:02:39 +0530

    add

--- backend/day2/server.js
+++ backend/day2/server.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 const app = express() //server created , or uska instance server constant me agya h 
 
-//not adding line to get some response on the client  side from the server
+
+//now adding line to get some response on the client  side from the server
 app.get('/', (req,res) => {
     res.send('hello world!!')
 })
+
+app.get('/about',(req,res)=>{
+    res.send('this is the about page')
+})
+
+app.get('/home',(req,res)=>{
+    res.send('this is the home page')
+})
+
 
 app.listen(3000) //server ko start krta hai 
 console.log("server started on port 3000")
