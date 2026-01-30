cd7b47ccd5400f9597fd0fa10b9bd60023f064d5
Atik Kumar <kumaratik424@gmail.com>
2026-01-30 18:05:31 +0530

    delete method

--- backend/day4/src/app.js
+++ backend/day4/src/app.js
@@ -15,6 +15,7 @@ app.get("/", (req,res) => {
     res.send("yoyo")
 })
 
+//post method api for /notes
 app.post("/notes", (req,res) => {
     console.log(req.body)
     notes.push(req.body)
@@ -22,6 +23,21 @@ app.post("/notes", (req,res) => {
     res.send("Notes Created Brother")
 })
 
+//get method api for /notes
+app.get("/notes", (req,res) => {
+    res.send(notes)
+})
 
+//delete method api to delete /notes
+/* since hume ni pata ki client konse notes ko delete karega to hum usse note ke index value ko dyanmic banate h : use kr ke 
+delete/ notes/:index says ki notes me _ index pe note h use delete kr do
+*/
+//also we are not puri taraf se deleting note , hum bs usse null se replace kr de rahe h 
+app.delete("/notes/:index",  (req,res) =>{
+    console.log(req.params.index)  //data ek single value hoti h ya bahut chooti hoti h to hum params ka use krte h ni to jab data jada ho to req.body ka use krte h 
+
+    delete notes[ req.params.index]
+    res.send("note deleted successfully")
+})  
 
 module.exports = app
