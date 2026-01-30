40035b2043eaed3949eff2ead43a2e1a15ca666f
Atik Kumar <kumaratik424@gmail.com>
2026-01-30 15:22:55 +0530

    using the server in a new file

--- backend/day4/server.js
+++ backend/day4/server.js
@@ -1,5 +1,7 @@
-
-const app = require("./src/app")
+/*use of server.js file
+-server ko start krna 
+*/
+const app = require("./src/app") //jo server app me create krke export kiya h usko yaha pe require kr rahe h use krne ke liye
 
 app.listen(3000, () => {
     console.log(`server is running on port $3000`)
