ec82e00123ed59bdd7445017cf6281f73373c778
Atik Kumar <kumaratik424@gmail.com>
2026-02-05 18:34:22 +0530

    uri hidding

--- backend/day6/server.js
+++ backend/day6/server.js
@@ -2,7 +2,7 @@
 //database se connect krna 
 //terminal pe mongoose install krna pe wo yaha ayega na ki app me 
 
-
+/* 
 const app = require("./src/app");
 const mongoose = require("mongoose")
 
@@ -19,3 +19,24 @@ connectToDb()
 app.listen(3000, () => {
     console.log("server has started on port 3000")
 })
+*/
+
+
+
+//Doing all this to hide my uri form github 
+require('dotenv').config();
+
+const app = require("./src/app");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to Database");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log("DB connection failed", err);
+  });
