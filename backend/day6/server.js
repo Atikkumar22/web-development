e25a229405d7bd4d024492bef4378c56b6114887
Atik Kumar <kumaratik424@gmail.com>
2026-02-05 18:36:33 +0530

    ..

--- backend/day6/server.js
+++ backend/day6/server.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose")
 
 function connectToDb(){
     //ye jo mon.con method h ye hamara express server ko mongoose DB connect krti h
-    mongoose.connect("mongodb+srv://kumaratik424_db_user:G2tet1Xpbg0Syg5A@cluster0.c8zvirg.mongodb.net/day6")
+    mongoose.connect(" (mongoDB uri here) /day6")
     .then(() => {
         console.log("Connected to Database")
     })
