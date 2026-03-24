4954bf32446fa81ace93e9adf7fe3d624a74eb4c
Atik Kumar <kumaratik424@gmail.com>
2026-03-24 17:50:07 +0530

    multer use and image upload to db

--- backend/day11/src/controllers/post.controller.js
+++ backend/day11/src/controllers/post.controller.js
@@ -1,7 +1,20 @@
 const postModel = require("../model/post.model")
+const imagekit = require("@imagekit/nodejs")
+const { toFile } = require("@imagekit/nodejs")
+
+const Imagekit = new imagekit({
+    privateKey: process.env.IMAGE_KIT
+})
 
 async function createPostController(req, res){
-    console.log(req.body)
+    console.log(req.body, req.file)
+
+    const file = await Imagekit.files.upload({
+        file: await toFile(Buffer.from(req.file.buffer), 'file'),
+        fileName: "Test"
+    })
+
+    res.send(file)
 }
 
 module.exports = {
--- backend/day11/src/routes/post.routes.js
+++ backend/day11/src/routes/post.routes.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const postRouter = express.Router()
 const postController = require("../controllers/post.controller")
+const multer = require("multer")
 
+// multer ka istemal hum isliye kare rahe h qki file jo client side se aati h usse server padh hi ni sakta  h to ussi liye hum multer ka use krte h taki jo files aa rahi h usse humara server oadh sake
+const upload = multer({ storage: multer.memoryStorage() })
 
-postRouter.post("/", postController.createPostController)
+// ye niche api / ke baad coma laga ke "upload.single("image")" likh rahe h taki humari file hume dikha sake or "image" FE se aani wali file ka naam h jo Postman me likha h 
+postRouter.post("/", upload.single("image"), postController.createPostController)
 
 module.exports = postRouter
