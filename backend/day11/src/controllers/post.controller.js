da3e7a088a4e5243d6980a345446ba7334d30833
Atik Kumar <kumaratik424@gmail.com>
2026-03-24 18:23:21 +0530

    feat: implement post creation with JWT authentication and image upload

--- backend/day11/src/controllers/post.controller.js
+++ backend/day11/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../model/post.model")
 const imagekit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken")
 
 const Imagekit = new imagekit({
     privateKey: process.env.IMAGE_KIT
@@ -9,12 +10,35 @@ const Imagekit = new imagekit({
 async function createPostController(req, res){
     console.log(req.body, req.file)
 
+    //different users agar file send kare server ko to ye check krne ke liye konse se user ki taraf se file ayi h hum tokens ka use karenge, like in login
+    const token = req.cookies.token
+
+    //file aate time agar token ni ata h to denial message , reason can be user ne reg. na kiya ho ya fir reg. ke yaad login na kiya ho to token expire hogya ho
+    if(!token){
+        return res.status(401).json({
+            message:"Token not provided , Unauthorized access"
+        })
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    // console.log(decoded)
+   
     const file = await Imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: "Test"
+        fileName: "Test",
+        folder: "cohort-insta-clone"
     })
 
-    res.send(file)
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({
+        message:"Post created successfully",
+        post
+    })
 }
 
 module.exports = {
--- backend/day11/src/routes/post.routes.js
+++ backend/day11/src/routes/post.routes.js
@@ -3,7 +3,7 @@ const postRouter = express.Router()
 const postController = require("../controllers/post.controller")
 const multer = require("multer")
 
-// multer ka istemal hum isliye kare rahe h qki file jo client side se aati h usse server padh hi ni sakta  h to ussi liye hum multer ka use krte h taki jo files aa rahi h usse humara server oadh sake
+// multer(middleware) ka istemal hum isliye kare rahe h qki file jo client side se aati h usse server padh hi ni sakta  h to ussi liye hum multer ka use krte h taki jo files aa rahi h usse humara server oadh sake
 const upload = multer({ storage: multer.memoryStorage() })
 
 // ye niche api / ke baad coma laga ke "upload.single("image")" likh rahe h taki humari file hume dikha sake or "image" FE se aani wali file ka naam h jo Postman me likha h 
